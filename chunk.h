62a7c9e3fe14a5539d72780c9612216f83d1ae23
Alan Lin <catwind7@gmail.com>
2024-08-30 10:59:59 -0400

    finish cahp 18 types

--- chunk.h
+++ chunk.h
@@ -6,12 +6,19 @@
 
 typedef enum {
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
   OP_RETURN,
   OP_NEGATE,
   OP_ADD,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
   OP_SUBTRACT,
   OP_MULTIPLY,
   OP_DIVIDE,
+  OP_NOT
 } OpCode; // by default uint8_t?
 
 typedef struct {
